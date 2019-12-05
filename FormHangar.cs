8d4edc30ada1e5d6ecd79e0f672edac709d5996e
Vikabraunone <vikabraunone@gmail.com>
2019-12-05 23:23:17 +0400

    Код 4 лабы

--- FormHangar.cs
+++ FormHangar.cs
@@ -7,15 +7,22 @@ namespace WindowsFormsBomber
     public partial class FormHangar : Form
     {
         /// <summary>
-        /// Объект от класса-ангара
+        /// Объект от класса многоуровневого ангара
         /// </summary>
-        Hangar<ITransport> hangar;
+        MultiLevelHangar hangar;
+        /// <summary>
+        /// Количество уровней-ангаров
+        /// </summary>
+        private const int countLevel = 5;
 
         public FormHangar()
         {
             InitializeComponent();
-            hangar = new Hangar<ITransport>(20, pictureBoxHangar.Width, pictureBoxHangar.Height);
-            Draw();
+            hangar = new MultiLevelHangar(countLevel, pictureBoxHangar.Width, pictureBoxHangar.Height);
+            //заполнение listBox
+            for (int i = 0; i < countLevel; i++)
+                listBoxLevels.Items.Add("Уровень " + (i + 1));
+            listBoxLevels.SelectedIndex = 0;
         }
 
         /// <summary>
@@ -23,10 +30,13 @@ namespace WindowsFormsBomber
         /// </summary>
         private void Draw()
         {
-            Bitmap bmp = new Bitmap(pictureBoxHangar.Width, pictureBoxHangar.Height);
-            Graphics gr = Graphics.FromImage(bmp);
-            hangar.Draw(gr);
-            pictureBoxHangar.Image = bmp;
+            if (listBoxLevels.SelectedIndex > -1)
+            {//если выбран один из пуктов в listBox 
+                Bitmap bmp = new Bitmap(pictureBoxHangar.Width, pictureBoxHangar.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                hangar[listBoxLevels.SelectedIndex].Draw(gr);
+                pictureBoxHangar.Image = bmp;
+            }
         }
 
         /// <summary>
@@ -36,12 +46,17 @@ namespace WindowsFormsBomber
         /// <param name="e"></param>
         private void ButtonSetWarPlane_Click(object sender, EventArgs e)
         {
-            ColorDialog dialog = new ColorDialog();
-            if (dialog.ShowDialog() == DialogResult.OK)
+            if (listBoxLevels.SelectedIndex > -1)
             {
-                var warPlane = new WarPlane(100, 1000, dialog.Color);
-                int place = hangar + warPlane;
-                Draw();
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    var warPlane = new WarPlane(100, 1000, dialog.Color);
+                    int place = hangar[listBoxLevels.SelectedIndex] + warPlane;
+                    if (place == -1)
+                        MessageBox.Show("Нет свободных мест", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Draw();
+                }
             }
         }
 
@@ -52,15 +67,20 @@ namespace WindowsFormsBomber
         /// <param name="e"></param>
         private void ButtonSetBomber_Click(object sender, EventArgs e)
         {
-            ColorDialog dialog = new ColorDialog();
-            if (dialog.ShowDialog() == DialogResult.OK)
+            if (listBoxLevels.SelectedIndex > -1)
             {
-                ColorDialog dialogDop = new ColorDialog();
-                if (dialogDop.ShowDialog() == DialogResult.OK)
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
                 {
-                    var bomber = new Bomber(100, 1000, dialog.Color, dialogDop.Color, true, true, true);
-                    int place = hangar + bomber;
-                    Draw();
+                    ColorDialog dialogDop = new ColorDialog();
+                    if (dialogDop.ShowDialog() == DialogResult.OK)
+                    {
+                        var bomber = new Bomber(100, 1000, dialog.Color, dialogDop.Color, true, true, true);
+                        int place = hangar[listBoxLevels.SelectedIndex] + bomber;
+                        if (place == -1)
+                            MessageBox.Show("Нет свободных мест", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Draw();
+                    }
                 }
             }
         }
@@ -72,24 +92,32 @@ namespace WindowsFormsBomber
         /// <param name="e"></param>
         private void ButtonGetWarPlane_Click(object sender, EventArgs e)
         {
-            if (maskedTextBoxPlace.Text != "")
+            if (listBoxLevels.SelectedIndex > -1)
             {
-                var warPlane = hangar - Convert.ToInt32(maskedTextBoxPlace.Text);
-                if (warPlane != null)
+                if (maskedTextBoxPlace.Text != "")
                 {
-                    Bitmap bmp = new Bitmap(pictureBoxWarPlane.Width, pictureBoxWarPlane.Height);
-                    Graphics gr = Graphics.FromImage(bmp);
-                    warPlane.SetPosition(5, 5, pictureBoxWarPlane.Width, pictureBoxWarPlane.Height);
-                    warPlane.DrawWarPlane(gr);
-                    pictureBoxWarPlane.Image = bmp;
+                    var warPlane = hangar[listBoxLevels.SelectedIndex] - Convert.ToInt32(maskedTextBoxPlace.Text);
+                    if (warPlane != null)
+                    {
+                        Bitmap bmp = new Bitmap(pictureBoxWarPlane.Width, pictureBoxWarPlane.Height);
+                        Graphics gr = Graphics.FromImage(bmp);
+                        warPlane.SetPosition(5, 5, pictureBoxWarPlane.Width, pictureBoxWarPlane.Height);
+                        warPlane.DrawWarPlane(gr);
+                        pictureBoxWarPlane.Image = bmp;
+                    }
+                    else
+                    {
+                        Bitmap bmp = new Bitmap(pictureBoxWarPlane.Width, pictureBoxWarPlane.Height);
+                        pictureBoxWarPlane.Image = bmp;
+                    }
+                    Draw();
                 }
-                else
-                {
-                    Bitmap bmp = new Bitmap(pictureBoxWarPlane.Width, pictureBoxWarPlane.Height);
-                    pictureBoxWarPlane.Image = bmp;
-                }
-                Draw();
             }
+        }
+
+        private void listBoxLevels_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Draw();
         }
     }
 }
